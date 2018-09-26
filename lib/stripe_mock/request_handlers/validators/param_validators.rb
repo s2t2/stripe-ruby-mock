d8cad946894a7d61a684cbae0b182abb0921e4d7
MJ Rossetti <datacreativellc@gmail.com>
2018-09-25 23:23:49 -0400

    Validate inclusion of plan currency

--- lib/stripe_mock/request_handlers/validators/param_validators.rb
+++ lib/stripe_mock/request_handlers/validators/param_validators.rb
@@ -48,56 +48,58 @@ module StripeMock
       # PlanValidator
       #
 
+      def missing_plan_amount_message
+        "Plans require an `amount` parameter to be set."
+      end
+
       SUPPORTED_PLAN_INTERVALS = ["month", "year", "week", "day"]
 
       def invalid_plan_interval_message
         "Invalid interval: must be one of month, year, week, or day"
       end
 
-      def validate_create_plan_params(params)
-        params[:id] = params[:id].to_s
+      SUPPORTED_CURRENCIES = [
+        "usd", "aed", "afn", "all", "amd", "ang", "aoa", "ars", "aud", "awg", "azn", "bam", "bbd", "bdt", "bgn",
+        "bif", "bmd", "bnd", "bob", "brl", "bsd", "bwp", "bzd", "cad", "cdf", "chf", "clp", "cny", "cop", "crc",
+        "cve", "czk", "djf", "dkk", "dop", "dzd", "egp", "etb", "eur", "fjd", "fkp", "gbp", "gel", "gip", "gmd",
+        "gnf", "gtq", "gyd", "hkd", "hnl", "hrk", "htg", "huf", "idr", "ils", "inr", "isk", "jmd", "jpy", "kes",
+        "kgs", "khr", "kmf", "krw", "kyd", "kzt", "lak", "lbp", "lkr", "lrd", "lsl", "mad", "mdl", "mga", "mkd",
+        "mmk", "mnt", "mop", "mro", "mur", "mvr", "mwk", "mxn", "myr", "mzn", "nad", "ngn", "nio", "nok", "npr",
+        "nzd", "pab", "pen", "pgk", "php", "pkr", "pln", "pyg", "qar", "ron", "rsd", "rub", "rwf", "sar", "sbd",
+        "scr", "sek", "sgd", "shp", "sll", "sos", "srd", "std", "svc", "szl", "thb", "tjs", "top", "try", "ttd",
+        "twd", "tzs", "uah", "ugx", "uyu", "uzs", "vnd", "vuv", "wst", "xaf", "xcd", "xof", "xpf", "yer", "zar",
+        "zmw", "eek", "lvl", "vef"
+      ]
 
-        @base_strategy.create_plan_params.keys.each do |name|
+      def invalid_currency_message(my_val)
+        "Invalid currency: #{my_val.downcase}. Stripe currently supports these currencies: #{SUPPORTED_CURRENCIES.join(", ")}"
+      end
+
+      def validate_create_plan_params(params)
+        plan_id = params[:id].to_s
+
+        @base_strategy.create_plan_params.keys.each do |attr_name|
           message =
-            if name == :amount
-              "Plans require an `#{name}` parameter to be set."
+            if attr_name == :amount
+              "Plans require an `#{attr_name}` parameter to be set."
             else
-              "Missing required param: #{name}."
+              "Missing required param: #{attr_name}."
             end
-          raise Stripe::InvalidRequestError.new(message, name) if params[name].nil?
+          raise Stripe::InvalidRequestError.new(message, attr_name) if params[attr_name].nil?
         end
 
-        if plans[ params[:id] ]
+        if plans[plan_id]
           raise Stripe::InvalidRequestError.new(already_exists_message(Stripe::Plan), :id)
         end
 
         unless params[:amount].integer?
           raise Stripe::InvalidRequestError.new(invalid_integer_message(params[:amount]), :amount)
         end
+
+        unless SUPPORTED_CURRENCIES.include?(params[:currency])
+          raise Stripe::InvalidRequestError.new(invalid_currency_message(params[:currency]), :currency)
+        end
       end
-
-      SUPPORTED_CURRENCIES = ["usd","aed", "afn", "all", "amd", "ang", "aoa", "ars", "aud", "awg", "azn", "bam", "bbd",
-                          "bdt", "bgn", "bif", "bmd", "bnd", "bob", "brl", "bsd", "bwp", "bzd", "cad", "cdf", "chf",
-                          "clp", "cny", "cop", "crc", "cve", "czk", "djf", "dkk", "dop", "dzd", "egp", "etb", "eur",
-                          "fjd", "fkp", "gbp", "gel", "gip", "gmd", "gnf", "gtq", "gyd", "hkd", "hnl", "hrk", "htg",
-                          "huf", "idr", "ils", "inr", "isk", "jmd", "jpy", "kes", "kgs", "khr", "kmf", "krw", "kyd",
-                          "kzt", "lak", "lbp", "lkr", "lrd", "lsl", "mad", "mdl", "mga", "mkd", "mmk", "mnt", "mop",
-                          "mro", "mur", "mvr", "mwk", "mxn", "myr", "mzn", "nad", "ngn", "nio", "nok", "npr", "nzd",
-                          "pab", "pen", "pgk", "php", "pkr", "pln", "pyg", "qar", "ron", "rsd", "rub", "rwf", "sar",
-                          "sbd", "scr", "sek", "sgd", "shp", "sll", "sos", "srd", "std", "svc", "szl", "thb", "tjs",
-                          "top", "try", "ttd", "twd", "tzs", "uah", "ugx", "uyu", "uzs", "vnd", "vuv", "wst", "xaf",
-                          "xcd", "xof", "xpf", "yer", "zar", "zmw", "eek", "lvl", "vef"
-                        ]
-
-      def invalid_currency_message(my_val)
-        "Invalid currency: #{my_val.downcase}. Stripe currently supports these currencies: #{SUPPORTED_CURRENCIES.join(", ")}"
-      end
-
-      def missing_plan_amount_message
-        "Plans require an `amount` parameter to be set."
-      end
-
-
 
 
 
